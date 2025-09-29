885e8d44208ed3e9f6ca26885982740aac9b0dae
sidfakenews986 <bisonsicolo@hotmail.com>
2025-09-29 15:06:18 +0100

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import Dunkelmodus from './dunkelmodus.js';
+
+const dunkelmodus = new Dunkelmodus({
+    buttonSelector: '#toggle-theme',
+    darkModeStyles: {
+        backgroundColor: '#121212',
+        color: '#ffffff'
+    },
+    lightModeStyles: {
+        backgroundColor: '#ffffff',
+        color: '#000000'
+    }
+});
